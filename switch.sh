e67f3fe1cd311d46ca2d741e6b4b60529841cec9
Thorsten Eggeling <Myria-de@users.noreply.github.com>
2016-02-15 20:26:57 +0000

    Update switch.sh

--- switch.sh
+++ switch.sh
@@ -3,7 +3,6 @@ date=`date "+%Y-%m-%d_%H:%M:%S"`
 FILE=~/Dropbox/IFTTT/Gmail/email.txt
 LOGFILE=~/switch.log
 if [ -f "$FILE" ];then
- LOGFILE=~/switch.log
  lines=($(cat $FILE))
   if [ "${lines[0]}" == "on" ]; then
    echo "Schalter an"
@@ -16,5 +15,5 @@ if [ -f "$FILE" ];then
    # Hier Befehl zur Steuerung etwa eines Schalters einbauen
    echo $date off >> $LOGFILE
   fi
-rm -rf $FILE
+ rm -rf $FILE
 fi
